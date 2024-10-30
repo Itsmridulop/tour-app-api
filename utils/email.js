ff88dfee5cf4f96a27c7d1516c9e1276d7b713af
Mridul Mishra <140900799+Itsmridulop@users.noreply.github.com>
2024-10-30 06:29:29 -0400

    add real smtp server

--- utils/email.js
+++ utils/email.js
@@ -2,8 +2,7 @@ const nodemailer = require('nodemailer')
 
 const sendEmail = options => {
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
+        service: 'gmail',
         auth: {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD
