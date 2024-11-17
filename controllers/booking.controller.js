731f8bb84ae55d2b7eac06cdc395c636184d9a2d
Mridul Mishra <140900799+Itsmridulop@users.noreply.github.com>
2024-11-17 08:02:25 -0500

    Update booking.controller.js

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -1,5 +1,27 @@
 const factory = require('./handlerFactory')
 const Booking = require('../model/booking.model')
+const catchAsync = require('../utils/catchAsync')
+const AppError = require('../utils/appError')
+
+exports.getBookingOfOneUser = catchAsync(async (req, res, next) => {
+    const booking = await Booking.find({ user: req.user.id })
+    if (!booking) next(new AppError('No bookings by this user.', 400))
+    res.status(200).json({
+        status: 'success',
+        result: booking.length,
+        data: booking
+    })
+})
+
+exports.getBookingOfUser = catchAsync(async (req, res, next) => {
+    const booking = await Booking.find({ user: req.params.id })
+    if (!booking) next(new AppError('No bookings by this user.', 400))
+    res.status(200).json({
+        status: 'success',
+        result: booking.length,
+        data: booking
+    })
+})
 
 exports.createBooking = factory.createOne(Booking)
 exports.getAllBooking = factory.getAll(Booking)
