ae92d6ac94d0c1fb50d7cc1dd3b9366679c56f5a
Mridul Mishra <140900799+Itsmridulop@users.noreply.github.com>
2024-11-17 08:01:59 -0500

    Update review.controller.js

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -1,10 +1,21 @@
 const Review = require('../model/review.model')
+const AppError = require('../utils/appError')
 const catchAsync = require('../utils/catchAsync')
 const factory = require('./handlerFactory')
 
 exports.getDailyReview = catchAsync(async (req, res, next) => {
     req.query.createdAt = req.query.date && Date.now()
     next()
+})
+
+exports.getReviewOfUser = catchAsync(async (req, res, next) => {
+    const reviews = await Review.find({ user: req.params.id })
+    if(!reviews) next(new AppError('This user dont reviewwed any tour', 400))
+    res.status(200).json({
+        status: 'success',
+        result: reviews.length,
+        data: reviews
+    })
 })
 
 exports.getAllReview = factory.getAll(Review)
