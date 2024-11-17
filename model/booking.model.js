b5d20d560779d73e60ea97552db2a9879891e59c
Mridul Mishra <140900799+Itsmridulop@users.noreply.github.com>
2024-11-17 08:00:39 -0500

    Update booking.model.js

--- model/booking.model.js
+++ model/booking.model.js
@@ -11,7 +11,7 @@ const BookingSchema = new Schema({
         ref: 'User',
         require: [true, 'A Booking must belongs to a user']
     },
-    numMember: {
+    members: {
         type: Number,
         require: [true, 'A Booking must have a number of members'],
         min: [1, 'Booking must a done for atleast one memeber']
@@ -20,6 +20,28 @@ const BookingSchema = new Schema({
         type: Date,
         default: Date.now()
     },
+    paidAt: {
+        type: Date
+    },
+    canceledAt: {
+        type: Date
+    },
+    paymentMethod: {
+        type: String,
+        enum: {
+            values: ['credit-card', 'cash', 'UPI'],
+            message: 'We accept only Credit Card, Cash and UPI for payment'
+        },
+        require: [true, 'Please select one of the payment methods']
+    },
+    status: {
+        type: String,
+        enum: {
+            values: ['booked', 'canceled', 'completed', 'paid', 'confirmed'],
+            message: 'Booking status must be either booked, canceled, paid, confirmed or completed'
+        },
+        default: 'booked'
+    },
     paid: {
         type: Boolean,
         default: false
@@ -27,6 +49,16 @@ const BookingSchema = new Schema({
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
+})
+
+BookingSchema.index({ tour: 1, user: 1 }, { unique: true })
+
+BookingSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'tour',
+        select: 'name startDates'
+    })
+    next()
 })
 
 module.exports = model('Booking', BookingSchema)
