d0768ca3ca89d97dd118f0cc026593953ce1c357
Mridul Mishra <140900799+Itsmridulop@users.noreply.github.com>
2024-11-17 08:01:08 -0500

    Update booking.route.js

--- routers/booking.route.js
+++ routers/booking.route.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const { createBooking, getAllBooking, getOneBooking, updateBooking, deleteBooking } = require('../controllers/booking.controller')
+const { createBooking, getAllBooking, getOneBooking, updateBooking, deleteBooking, getBookingOfOneUser, getBookingOfUser } = require('../controllers/booking.controller')
+const { protect } = require('../controllers/auth.controller')
 
 const router = express.Router()
 
-router.post('/',createBooking)
+router.use(protect)
+router.post('/', createBooking)
+router.get('/user', getBookingOfOneUser)
+router.get('/user/:id', getBookingOfUser)
 router.get('/', getAllBooking)
 router.get('/:id', getOneBooking)
 router.patch('/:id', updateBooking)
