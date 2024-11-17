59f2f8114bc5ad91d12b7824ff3cbf9293571a78
Mridul Mishra <140900799+Itsmridulop@users.noreply.github.com>
2024-11-17 08:01:28 -0500

    Update review.router.js

--- routers/review.router.js
+++ routers/review.router.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const { protect, restrictTo } = require('../controllers/auth.controller')
-const { getAllReview, createReview, updateReview, deleteReview, getDailyReview } = require('../controllers/review.controller')
+const { getAllReview, createReview, updateReview, deleteReview, getDailyReview, getReviewOfUser } = require('../controllers/review.controller')
 
 const router = express.Router({ mergeParams: true })
 
 router.use(protect)
 // router.get('/dailyReviews', restrictTo('admin', 'lead-guide'), getDailyReview, getAllReview)
+router.get('/user/:id', restrictTo('admin', 'lead-guide'), getReviewOfUser)
 router.get('/', restrictTo('admin', 'lead-guide'), getAllReview)
 router.post('/', restrictTo('user'), createReview)
 router.delete('/:id', restrictTo('user'), deleteReview)
