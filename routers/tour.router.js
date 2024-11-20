01f72264eb92d6d36490c9d8c2bf4148708958a5
Mridul Mishra <140900799+Itsmridulop@users.noreply.github.com>
2024-11-19 22:58:14 -1100

    Update tour.router.js

--- routers/tour.router.js
+++ routers/tour.router.js
@@ -12,11 +12,11 @@ router.get('/top-5-tours', topTours, getTours)
 router.get('/tour-within/:distance/center/:latlng/unit/:unit', gettoursWithin)
 router.use(protect)
 router.get('/distance', getDistance)
-router.post('/', restrictTo('admin', 'lead-guide'), uploadTourImage, resizeTourImage, createTour)
+router.post('/', restrictTo('admin', 'lead-guide'), uploadTourImage, resizeTourImage, FindGuideId, createTour)
 router.get('/stats', restrictTo('admin'), tourStats)
 router.get('/monthly-plan', restrictTo('admin', 'lead-guide', 'guide'), getMonthlyPlan)
 router.get('/:id', getOneTour)
 router.delete('/:id', restrictTo('admin', 'lead-guide'), deleteTour)
-router.patch('/:id', restrictTo('admin', 'lead-guide'), uploadTourImage, resizeTourImage, FindGuideId,updateTour)
+router.patch('/:id', restrictTo('admin', 'lead-guide'), uploadTourImage, resizeTourImage, FindGuideId, updateTour)
 
 module.exports = router
